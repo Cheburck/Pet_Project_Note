792eb91e5f6e8796b46ffc84d6c7d090d300cf05
Cheburck <145753275+Cheburck@users.noreply.github.com>
2024-09-18 18:21:21 +0300

    fix error

--- frontend/src/components/CreateNoteForm.jsx
+++ frontend/src/components/CreateNoteForm.jsx
@@ -10,26 +10,6 @@ export default function CreateNoteForm({ onCreate }) {
 		onCreate(note);
 	};
 
-	const [text, setText] = useState('');
-
-	const handleKeyDown = (event) => {
-		// Проверяем, нажаты ли Ctrl или Shift вместе с Tab
-		if (event.key === 'Tab' && event.ctrlKey) {
-		event.preventDefault(); // Предотвращаем стандартное поведение
-		const start = event.target.selectionStart;
-		const end = event.target.selectionEnd;
-
-		// Устанавливаем новый текст с символом табуляции
-		const newText = text.substring(0, start) + '\t' + text.substring(end);
-		setText(newText);
-
-		// Устанавливаем курсор после вставленного символа табуляции
-		setTimeout(() => {
-			event.target.selectionStart = event.target.selectionEnd = start + 1;
-		}, 0);
-		}
-	};
-
 	return (
 		<form onSubmit={onSubmit} className='w-full flex flex-col gap-3'>
 			<h3 className='font-bold text-xl'>Создание заметки</h3>
@@ -39,7 +19,6 @@ export default function CreateNoteForm({ onCreate }) {
 				onChange={(e) => setNote({ ...note, title: e.target.value })}
 			/>
 			<Textarea
-				onKeyDown={handleKeyDown}
 				placeholder='Описание'
 				value={note?.description ?? ''}
 				onChange={(e) => setNote({ ...note, description: e.target.value })}
